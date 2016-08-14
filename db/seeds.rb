f930e18c37f149cb024b5e2cb265fded5ddd7b66
maxrueb <maxrueb@yahoo.de>
2016-08-14 14:33:26 +0200

    adding products

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Product.create(:name => "seed-generated product", :description => "I also created this product without using the HTML form!")
+Product.create(:name => 'Racing Bike', :description => 'The must have for every biker who likes it fast.', :image_url => 'https://images-eu.ssl-images-amazon.com/images/I/81XfDlW2yRL._SL1500_.jpg', :color => 'Red')
+Product.create(:name => 'City Bike', :description => 'Good for every situation in the city. With every features for a safe ride', :image_url => 'https://momentummag.com/wp-content/uploads/2015/01/Sommer-red-high-res-RESIZED.jpg', :color => 'Orange')
+Product.create(:name => 'Mountainbike', :description => 'The perfect bike for ambitious bikers. Perfect for intensive mountain tours', :image_url => 'http://www.fahrrad.de/fileadmin/mediapool/fde/mountainbike.jpg', :color => 'Black')
