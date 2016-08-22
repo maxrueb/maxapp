46311b84eaa4d13497d0780134323a529ccc93d2
maxrueb <maxrueb@yahoo.de>
2016-08-22 19:32:10 +0200

    5.3 sendgrid setup

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,20 @@
 class StaticPagesController < ApplicationController
   def index
   end
-end
-
-
-class StaticPagesController < ApplicationController
 
   def landing_page
     @featured_product = Product.first
     @products = Product.limit(3)
   end
+
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(:from => @email,
+      :to => 'app54709579@heroku.com',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+	end
 end
+
