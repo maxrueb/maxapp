c3c5ffb3c1219310dc72bb61fe3a7b3c80600c55
maxrueb <maxrueb@yahoo.de>
2016-08-24 17:46:46 +0200

    5.4 Authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160815131913) do
+ActiveRecord::Schema.define(version: 20160824152959) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20160815131913) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "color"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
