b0f60ea94ffc613e701def800f75512c0a774e0d
maxrueb <maxrueb@yahoo.de>
2016-08-30 19:35:32 +0200

    5.9 pagination

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+
 	def create
 	    @product = Product.find(params[:product_id])
 	    @comment = @product.comments.new(comment_params)
@@ -27,3 +28,4 @@ class CommentsController < ApplicationController
   	  params.require(:comment).permit(:user_id, :body, :rating)
  	end
 end
+		
