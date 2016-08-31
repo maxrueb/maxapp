159a9656c281e8c759b45b6cff72d5c2c7a8b5a0
maxrueb <maxrueb@yahoo.de>
2016-08-31 15:40:50 +0200

    5.9 pagination

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,7 +15,7 @@ end
   # GET /products/1
   # GET /products/1.json
   def show
-    @comments = @product.comments.order("created_at DESC").paginate(:page => params[:page], :per_page => 3)
+    @comments = @product.comments.order("created_at DESC").paginate(:page => params[:page], :per_page => 2)
     @products = Product.all
   end
 
