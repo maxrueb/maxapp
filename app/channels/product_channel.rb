b43b29b3cbbd408111de609b0479f7bcd890e0e4
maxrueb <maxrueb@yahoo.de>
2016-09-14 16:14:55 +0200

    real-time updates with ActionCable

--- app/channels/product_channel.rb
+++ app/channels/product_channel.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class ProductChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "some_channel"
+    stream_from "product_channel"
   end
 
   def unsubscribed
